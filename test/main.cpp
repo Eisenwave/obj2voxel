43c67c6783666c4621398de6f8349517dcc322bf
Eisenwave <me@eisenwave.net>
2021-03-05 00:10:54 +0100

    greatly expand test utilities and implement unit cube voxelization test

--- test/main.cpp
+++ test/main.cpp
@@ -1,24 +1,25 @@
 #include "obj2voxel.h"
 #include "testutil.hpp"
 
+#include "voxelio/format/vl32.hpp"
 #include "voxelio/log.hpp"
 
 #include <vector>
+
+std::vector<NamedTest> tests;
 
 namespace {
 
 // TESTS ===============================================================================================================
 
-using TestFunction = void (*)(void);
-
-void test_errorOnMissingInput()
+TEST(test_errorOnMissingInput)
 {
     pushLogLevel(OBJ2VOXEL_LOG_LEVEL_SILENT);
 
-    size_t voxelCount = 0;
+    CountingOutput output;
 
     obj2voxel_instance *instance = obj2voxel_alloc();
-    obj2voxel_set_output_callback(instance, &callback::countVoxels, &voxelCount);
+    obj2voxel_set_output_callback(instance, &outputCallback<CountingOutput>, &output);
     obj2voxel_set_resolution(instance, 1);
     obj2voxel_error_t result = obj2voxel_voxelize(instance);
     obj2voxel_free(instance);
@@ -28,16 +29,16 @@ void test_errorOnMissingInput()
     VXIO_ASSERT_EQ(result, OBJ2VOXEL_ERR_NO_INPUT);
 }
 
-void test_errorOnMissingOutput()
+TEST(test_errorOnMissingOutput)
 {
     constexpr const float vertices[9]{0, 0, 0, 0, 0, 1, 1, 0, 0};
 
     pushLogLevel(OBJ2VOXEL_LOG_LEVEL_SILENT);
 
-    TriangleIterator iterator{vertices, 1};
+    TriangleInput input{vertices, 3};
 
     obj2voxel_instance *instance = obj2voxel_alloc();
-    obj2voxel_set_input_callback(instance, &callback::callNext<TriangleIterator>, &iterator);
+    obj2voxel_set_input_callback(instance, &inputCallback<TriangleInput>, &input);
     obj2voxel_set_resolution(instance, 1);
     obj2voxel_error_t result = obj2voxel_voxelize(instance);
     obj2voxel_free(instance);
@@ -47,18 +48,18 @@ void test_errorOnMissingOutput()
     VXIO_ASSERT_EQ(result, OBJ2VOXEL_ERR_NO_OUTPUT);
 }
 
-void test_errorOnMissingResolution()
+TEST(test_errorOnMissingResolution)
 {
     constexpr const float vertices[9]{0, 0, 0, 0, 0, 1, 1, 0, 0};
 
     pushLogLevel(OBJ2VOXEL_LOG_LEVEL_SILENT);
 
-    TriangleIterator iterator{vertices, 1};
-    size_t voxelCount = 0;
+    TriangleInput input{vertices, 3};
+    CountingOutput output;
 
     obj2voxel_instance *instance = obj2voxel_alloc();
-    obj2voxel_set_input_callback(instance, &callback::callNext<TriangleIterator>, &iterator);
-    obj2voxel_set_output_callback(instance, &callback::countVoxels, &voxelCount);
+    obj2voxel_set_input_callback(instance, &inputCallback<TriangleInput>, &input);
+    obj2voxel_set_output_callback(instance, &outputCallback<CountingOutput>, &output);
     obj2voxel_error_t result = obj2voxel_voxelize(instance);
     obj2voxel_free(instance);
 
@@ -67,24 +68,71 @@ void test_errorOnMissingResolution()
     VXIO_ASSERT_EQ(result, OBJ2VOXEL_ERR_NO_RESOLUTION);
 }
 
-// MAIN ================================================================================================================
-
-struct NamedTest {
-    const char *name;
-    TestFunction test;
+// clang-format off
+constexpr float unitCubeVertices[8 * 3] {
+    0, 0, 0,
+    0, 0, 1,
+    0, 1, 0,
+    0, 1, 1,
+    1, 0, 0,
+    1, 0, 1,
+    1, 1, 0,
+    1, 1, 1
 };
+
+constexpr size_t unitCubeElements[6 * 4] {
+    0, 1, 3, 2,
+    4, 6, 7, 5,
+    0, 4, 5, 1,
+    2, 3, 7, 6,
+    0, 2, 6, 4,
+    1, 5, 7, 3
+};
+// clang-format on
+
+TEST(test_unitCubeProducesExpectedVoxelCount)
+{
+    constexpr size_t resolution = 64;
+
+    IndexedQuadInput input{unitCubeVertices, unitCubeElements, sizeof(unitCubeElements) / sizeof(size_t)};
+
+    obj2voxel_instance *instance = obj2voxel_alloc();
+
+#ifdef DUMP_OUTPUTS
+    std::optional<voxelio::FileOutputStream> outStream = voxelio::openForWrite("/tmp/obj2voxel_test.vl32", voxelio::OpenMode::BINARY);
+    VXIO_ASSERT(outStream.has_value());
+    voxelio::vl32::Writer writer{*outStream};
+    VoxelioOutput output{writer};
+    obj2voxel_set_output_callback(instance, &outputCallback<VoxelioOutput>, &output);
+#else
+    CountingOutput output;
+    obj2voxel_set_output_callback(instance, &outputCallback<CountingOutput>, &output);
+#endif
+
+    obj2voxel_set_input_callback(instance, &inputCallback<IndexedQuadInput>, &input);
+    obj2voxel_set_resolution(instance, resolution);
+    obj2voxel_error_t result = obj2voxel_voxelize(instance);
+    obj2voxel_free(instance);
+
+    VXIO_ASSERT_EQ(result, OBJ2VOXEL_ERR_OK);
+
+
+    constexpr size_t expectedVertexVoxels = 8;
+    constexpr size_t expectedEdgeVoxels = 12 * (resolution - 2);
+    constexpr size_t expectedFaceVoxels = 6 * (resolution - 2) * (resolution - 2);
+    constexpr size_t expectedVoxels = expectedVertexVoxels + expectedEdgeVoxels + expectedFaceVoxels;
+
+    VXIO_ASSERT_EQ(output.voxelCount, expectedVoxels);
+}
+
+// MAIN ================================================================================================================
 
 }  // namespace
 
 int main()
 {
-#define NAMED_TEST(name) NamedTest{#name, &name}
 
     obj2voxel_set_log_level(OBJ2VOXEL_LOG_LEVEL_DEBUG);
-
-    std::vector<NamedTest> tests{NAMED_TEST(test_errorOnMissingInput),
-                                 NAMED_TEST(test_errorOnMissingOutput),
-                                 NAMED_TEST(test_errorOnMissingResolution)};
 
     VXIO_LOG(INFO, "Running " + voxelio::stringify(tests.size()) + " tests ...");
 
--- test/testutil.hpp
+++ test/testutil.hpp
@@ -3,7 +3,11 @@
 
 #include "obj2voxel.h"
 
+#include "voxelio/assert.hpp"
+#include "voxelio/voxelio.hpp"
+
 #include <vector>
+#include <cstring>
 
 // CONFIG ==============================================================================================================
 
@@ -33,41 +37,141 @@ void popLogLevel()
 }
 #endif
 
-struct TriangleIterator {
+// INPUTS ==============================================================================================================
+
+struct TriangleInput {
     const float *vertices;
-    size_t triangleCount;
+    size_t vertexCount;
 
-    size_t index = 0;
+    size_t vertexIndex = 0;
 
-    TriangleIterator(const float *vertices, size_t triangleCount) : vertices{vertices}, triangleCount{triangleCount} {}
+    TriangleInput(const float *vertices, size_t vertexCount) : vertices{vertices}, vertexCount{vertexCount} {
+        VXIO_ASSERT_EQ(vertexCount % 3, 0);
+    }
 
     bool next(obj2voxel_triangle *triangle)
     {
         constexpr const size_t floatsPerTriangle = 9;
 
-        if (index++ >= triangleCount) {
+        if (vertexIndex >= vertexCount) {
             return false;
         }
-        obj2voxel_set_triangle_basic(triangle, vertices + index * floatsPerTriangle);
+
+        vertexIndex += 3;
+        obj2voxel_set_triangle_basic(triangle, vertices + vertexIndex * floatsPerTriangle);
         return true;
     }
 };
 
-namespace callback {
+template <size_t PRIM_VERTICES, std::enable_if_t<PRIM_VERTICES == 3 || PRIM_VERTICES == 4, int> = 0>
+struct IndexedPrimitiveInput {
+    const float *vertices;
+    const size_t *elements;
+    size_t elementCount;
+    size_t elementIndex = 0;
+
+    float buffer[9];
+
+    IndexedPrimitiveInput(const float *vertices, const size_t *elements, size_t elementCount)
+        : vertices{vertices}, elements{elements}, elementCount{elementCount}
+    {
+    }
+
+    bool next(obj2voxel_triangle *triangle)
+    {
+        if (elementIndex >= elementCount) {
+            return false;
+        }
+
+        // To output the triangles of a quad, we switch between cases A and B between each iteration.
+        // We can tell the case by the lowest bit of the element index.
+        // For PRIM_VERTICES == 3, there is only case A.
+        //
+        // 0--1     A. output 0, 1, 2 (else-block)
+        // |  |     B. output 2, 3, 0 (true-block)
+        // 3--2
+        if (PRIM_VERTICES == 4 && (elementIndex & 1)) {
+            bufferElementVertex(0, -1);
+            bufferElementVertex(3, 0);
+            bufferElementVertex(6, -3);
+            elementIndex += 1;
+        }
+        else {
+            bufferElementVertex(0, 0);
+            bufferElementVertex(3, 1);
+            bufferElementVertex(6, 2);
+            elementIndex += 3;
+        }
+
+        obj2voxel_set_triangle_basic(triangle, buffer);
+        return true;
+    }
+
+    void bufferElementVertex(unsigned bufferOffset, int elementOffset)
+    {
+        std::memcpy(buffer + bufferOffset, vertices + elements[elementIndex + elementOffset] * 3, sizeof (float) * 3);
+    }
+};
+
+using IndexedTriangleInput = IndexedPrimitiveInput<3>;
+using IndexedQuadInput = IndexedPrimitiveInput<4>;
+
+// OUTPUTS =============================================================================================================
+
+struct CountingOutput {
+    size_t voxelCount = 0;
+
+    bool write(uint32_t *, size_t voxelCount)
+    {
+        this->voxelCount += voxelCount;
+        return true;
+    }
+};
+
+struct VoxelioOutput {
+    voxelio::AbstractListWriter &writer;
+    size_t voxelCount = 0;
+
+    bool write(uint32_t *rawBuffer, size_t voxelCount)
+    {
+        this->voxelCount += voxelCount;
+        /// Trust me, it's safe :)
+        auto *voxelBuffer = reinterpret_cast<voxelio::Voxel32*>(rawBuffer);
+        voxelio::ResultCode result = writer.write(voxelBuffer, voxelCount);
+        return voxelio::isGood(result);
+    }
+
+};
 
 template <typename T>
-bool callNext(void *iter, obj2voxel_triangle *triangle)
+bool inputCallback(void *iter, obj2voxel_triangle *triangle)
 {
     return reinterpret_cast<T *>(iter)->next(triangle);
 }
 
-bool countVoxels(void *counter, uint32_t *, size_t voxelCount)
+template <typename T>
+bool outputCallback(void* sink, uint32_t *voxelBuffer, size_t voxelCount)
 {
-    size_t &typedCounter = *reinterpret_cast<size_t *>(counter);
-    typedCounter += voxelCount;
-    return true;
+    return reinterpret_cast<T*>(sink)->write(voxelBuffer, voxelCount);
 }
 
-}  // namespace callback
+// TEST METAPROGRAMMING ================================================================================================
+
+
+using TestFunction = void (*)(void);
+
+struct NamedTest {
+    const char *name;
+    TestFunction test;
+};
+
+/// Stores all tests for execution.
+extern std::vector<NamedTest> tests;
+
+#define TEST(name) \
+    void name(); \
+    static int name##__ = [](){tests.push_back({#name, &name}); \
+    return 0;}(); \
+    void name()
 
 #endif
